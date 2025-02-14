e6ed75265d28969eadef2348e52ef8e0b4d85a80
ඔබ පරශලක නමය < GitHub  >
2025-02-15 01:37:44 +0530

    script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('profile-img').addEventListener('mouseover', function() {
     this.style.transition = 'transform 1s ease-in-out';
-    this.style.transform = 'translateX(-100%) rotate(360deg)';
+    this.style.transform = 'translateX(150%) rotate(360deg)';
 });
 document.getElementById('profile-img').addEventListener('mouseleave', function() {
     this.style.transition = 'transform 1s ease-in-out';
